e8b70ff272bdbeb49c0103e5ca6be47715b89642
macbookpro <macbookpro@192.168.100.81>
2024-03-08 19:16:26 +0300

    class customer

--- project113/src/project113/Customer.java
+++ project113/src/project113/Customer.java
@@ -2,5 +2,20 @@ package project113;
 
 public class Customer {
 	
+	private int cID ;
+	private String name ;
+	private String phoneNo ;
+	
+	public Customer (int id , String n , String phNo) {
+		
+		cID = id ;
+		name = n ;
+		phoneNo = phNo ;
+	}
+	
+	public void display() {
+		
+		System.out.println(" Customer Name: " + name + ", ID IS: "+ cID + ", Phone Number: "+ phoneNo + "\n");
+	}
 	
 }
