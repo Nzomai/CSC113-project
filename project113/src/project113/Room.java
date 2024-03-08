6cec3932a6a36e8bb0465f80f299a9fd3bd7d5dc
Ramaalomair <162565233+Ramaalomair@users.noreply.github.com>
2024-03-08 18:12:43 +0300

    Update Room.java

--- project113/src/project113/Room.java
+++ project113/src/project113/Room.java
@@ -1,5 +1,41 @@
 package project113;
 
 public class Room  {
+  package project113;
+
+public abstract class Room {
+	
+	protected int RoomNo;
+	protected int floorNo;
+	protected boolean Available;
+	protected boolean view;
+	protected int checkIn;
+	protected int checkOut;
+	
+	public Room(int rNo, int fNo, boolean view, int chIn, int chOut) {
+		RoomNo = rNo;
+		floorNo = fNo;
+		this.view = view;
+		checkIn = chIn;
+		checkOut = chOut;
+	}
+	
+	public abstract double calculatePrice();
+	
+	public int duration() {
+		return checkOut - checkIn;
+	}
+	
+	public String toString() {
+		String s = "Room Number: " + RoomNo +"\n Floor Number: " + floorNo ;
+		s += "\n View: " + view + "\n Number of Days: " + duration();
+		return s;
+	}
+	
+	
+	
+
+}
+
 
 }
