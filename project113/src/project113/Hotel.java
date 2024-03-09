917285688975b097caa012ad8fabbcd239590c60
macbookpro <macbookpro@localhost>
2024-03-09 16:05:26 +0300

    Hotel TestHotel

--- project113/src/project113/Hotel.java
+++ project113/src/project113/Hotel.java
@@ -8,109 +8,100 @@ public Room [] LisOfRooms;
 
 
 
-public Hotel(String hName, String adress, int size) {
-	this.hName = hName;
-	Adress = adress;
-	nOfRooms = 0;
-	LisOfRooms = new Room [size];
-	}
-	
-	public Suite[] DisplayAvailableSuite() {
-		Suite SuiteList [] = new Suite [nOfRooms];
-		 int s = 0;
-		 for (int i = 0 ; i<nOfRooms ; i++)
-			 if (LisOfRooms [i] instanceof Suite) {
-				 SuiteList [s] = new Suite ((Suite) LisOfRooms [i]);
-				 s++;
-			 }
-	if (s == 0)
-		return null;
-		else			
-		 return SuiteList;
-		 
-		
-	}
-	
-	
-	public Single[] DisplayAvailableSingle() {
-		Single SingleList [] = new Single [nOfRooms];
-		 int a = 0;
-		 for (int i = 0 ; i<nOfRooms ; i++)
-			 if (LisOfRooms [i] instanceof Single) {
-				 SingleList [a] = new Single ((Single) LisOfRooms [i]);
-				 a++;
-			 }
-	if (a == 0)
-		return null;
-		else			
-		 return SingleList;
-		 
-		
-	}
-	
-	
-	public String toString () {
-		
-		String str = "Hotel Name is = " + this.hName + "Number of Rooms =" +this.nOfRooms +"Adress = "+this.Adress +"\n";
-		str+= "============================\n";
-		for (int i = 0 ; i<nOfRooms ; i++) 
-			str += LisOfRooms [i].toString() + "\n";
-			
-			return str;
-		}
+public Hotel(String hName, String adress) {
+this.hName = hName;
+Adress = adress;
+nOfRooms = 0;
+LisOfRooms = new Room [100];
+}
 
-	public String gethName() {
-		return hName;
-	}
+public Suite[] DisplayAvailableSuite() {
+Suite SuiteList [] = new Suite [nOfRooms];
+int s = 0;
+for (int i = 0 ; i<nOfRooms ; i++)
+if (LisOfRooms [i] instanceof Suite) {
+SuiteList [s] = new Suite ((Suite) LisOfRooms [i]);
+s++;
+}
+if (s == 0)
+return null;
+else 
+return SuiteList;
+}
 
-	public void sethName(String hName) {
-		this.hName = hName;
-	}
+public Single[] DisplayAvailableSingle() {
+Single SingleList [] = new Single [nOfRooms];
+int a = 0;
+for (int i = 0 ; i<nOfRooms ; i++)
+if (LisOfRooms [i] instanceof Single) {
+SingleList [a] = new Single ((Single) LisOfRooms [i]);
+a++;
+}
+if (a == 0)
+return null;
+else 
+return SingleList;
+}
 
-	public String getAdress() {
-		return Adress;
-	}
+public boolean addRoom (Room R) {
+if (this.nOfRooms<LisOfRooms.length )
+{
+if (R instanceof Suite)
+LisOfRooms [nOfRooms++] = new Suite ((Suite) R);
+else
+if (R instanceof Single)
+LisOfRooms [nOfRooms++] = new Single ((Single) R);
+return true;
+}
+return false;
+} 
 
-	public void setAdress(String adress) {
-		Adress = adress;
-	}
 
-	public int getnOfRooms() {
-		return nOfRooms;
-	}
-
-	public void setnOfRooms(int nOfRooms) {
-		this.nOfRooms = nOfRooms;
-	}
-
-	public Room[] getLisOfRooms() {
-		return LisOfRooms;
-	}
-
-	public void setLisOfRooms(Room[] lisOfRooms) {
-		LisOfRooms = lisOfRooms;
-	}
-		
-		 
-	
-	
-	
-	
-	
+public boolean findAvailableRoom (int NRoom) {
+for (int i = 0 ; i<nOfRooms ; i++)
+if (LisOfRooms [i].getRoomNo() == NRoom && LisOfRooms[i].isAvailable() )
+return true;
+return false;
 }
 
 
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+public String toString () {
+String str = "Hotel Name is = " + this.hName + "Number of Rooms =" +this.nOfRooms +"Adress = "+this.Adress +"\n";
+str+= "============================\n";
+for (int i = 0 ; i<nOfRooms ; i++) 
+str += LisOfRooms [i].toString() + "\n";
+return str;
+}
+
+public String gethName() {
+return hName;
+}
+
+public void sethName(String hName) {
+this.hName = hName;
+}
+
+public String getAdress() {
+return Adress;
+}
+
+public void setAdress(String adress) {
+Adress = adress;
+}
+
+public int getnOfRooms() {
+return nOfRooms;
+}
+
+public void setnOfRooms(int nOfRooms) {
+this.nOfRooms = nOfRooms;
+}
+
+public Room[] getLisOfRooms() {
+return LisOfRooms;
+}
+
+public void setLisOfRooms(Room[] lisOfRooms) {
+LisOfRooms = lisOfRooms;
+}
 }
